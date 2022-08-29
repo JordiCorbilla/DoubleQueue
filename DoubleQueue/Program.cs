0466a926836d536012347f013e7dc90bd046b37a
Jordi Corbilla <jordi.coll.corbilla@gmail.com>
2022-08-29 13:44:45 +0100

    adding package reference

--- DoubleQueue/Program.cs
+++ DoubleQueue/Program.cs
@@ -7,11 +7,14 @@
             var queue = new DeQueue<int>();
             queue.Enqueue(0);
             queue.Enqueue(10);
+            Console.WriteLine($"Peek Left: {queue.PeekLeft()}");
+            Console.WriteLine($"Peek Right: {queue.PeekRight()}");
             queue.Enqueue(20);
             queue.Enqueue(30);
             queue.Enqueue(40);
             queue.Enqueue(50);
 
+            Console.WriteLine("Dequeueing");
             Console.WriteLine(queue.DequeueLeft());
             Console.WriteLine(queue.Dequeue());
             Console.WriteLine(queue.Dequeue());
