99499bfedefe7086db47ae9a017574905297ad60
Jordi Corbilla <jordi.coll.corbilla@gmail.com>
2022-08-29 14:06:39 +0100

    add test project

---
+++ DoubleQueue.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace DoubleQueue.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
